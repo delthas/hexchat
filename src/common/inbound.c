b27845af185e1a1efc1cdd1eaf7929328320daaa
delthas <delthas@dille.cc>
2020-11-08 22:43:51 +0100

    Close tabs when parting unfocused, read channels
    When receiving an inbound PART for the current user ("You Part"), close
    the channel tab if the channel is not focused, and there is no pending
    message to be read.

--- src/common/inbound.c
+++ src/common/inbound.c
@@ -654,13 +654,21 @@ inbound_upart (server *serv, char *chan, char *ip, char *reason,
 	session *sess = find_channel (serv, chan);
 	if (sess)
 	{
-		if (*reason)
-			EMIT_SIGNAL_TIMESTAMP (XP_TE_UPARTREASON, sess, serv->nick, ip, chan,
-										  reason, 0, tags_data->timestamp);
-		else
-			EMIT_SIGNAL_TIMESTAMP (XP_TE_UPART, sess, serv->nick, ip, chan, NULL,
-										  0, tags_data->timestamp);
+		gboolean close = !(sess->tab_state & TAB_STATE_NEW_MSG) && sess != current_tab;
+		if (!close)
+		{
+			if (*reason)
+				EMIT_SIGNAL_TIMESTAMP (XP_TE_UPARTREASON, sess, serv->nick, ip, chan,
+									   reason, 0, tags_data->timestamp);
+			else
+				EMIT_SIGNAL_TIMESTAMP (XP_TE_UPART, sess, serv->nick, ip, chan, NULL,
+									   0, tags_data->timestamp);
+		}
 		clear_channel (sess);
+		if (close)
+		{
+			fe_close_window (sess);
+		}
 	}
 }
 
